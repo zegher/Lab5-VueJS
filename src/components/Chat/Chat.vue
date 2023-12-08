f155ae6cf7a3010a58cec752fdd1ce93a9e4c3b8
zghr <r0878962@student.thomasmore.be>
2023-12-08 22:12:24 +0100

    Feat: added comments

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -2,14 +2,39 @@
     //import ref
     import { ref, reactive, onMounted } from 'vue';
     let message = ref(""); //int, string, boolean
-    let allMessages = reactive({
-        data: ["Goedemorgen!", "Goedemiddag!", "Goedenavond!"],
-    }); 
+
 
     const sendMessage = () => {
         allMessages.data.push(message.value);
         message.value = "";
     };
+
+    //fetch user and message from https://lab5-p379.onrender.com/api/v1/messages/ and put messages in allMessages.data
+  
+        let allMessages = reactive({
+            data: [],
+        });
+
+        onMounted(() => {fetch("https://lab5-p379.onrender.com/api/v1/messages/")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+
+          // Check if the "user" property exists in the API response before assigning it
+          if (data.user) {
+            allMessages.data = [{ user: data.user, text: data.text }];
+          }
+
+          // If the API response is an array, loop through it
+          if (Array.isArray(data)) {
+            allMessages.data = data.map((message) => ({
+              user: message.user,
+              text: message.text,
+            }));
+          }
+        });
+    });
+
 </script>
 
 <template>
